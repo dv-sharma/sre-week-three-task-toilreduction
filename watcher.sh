0aa33ab51d44e2708d4096af1a2f0d5d13f739f2
dv-sharma <65087388+dv-sharma@users.noreply.github.com>
2024-04-24 20:41:41 +0000

    script added

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,20 @@
-#TODO
+#!/bin/bash
+NAMESPACE="sre"
+DEPLOYMENT_NAME="swype-app"
+MAX_RESTART=3
+
+while true; do 
+    RESTART=$(kubectl get pod  -l app=${DEPLOYMENT_NAME} -n ${NAMESPACE} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+    echo "Restart count: ${RESTART}"
+    echo "${MAX_RESTART}"
+
+        if [ $RESTART -gt $MAX_RESTART ]; then
+            echo "Maximum restarts exceeded. Scaling down the deployment gracefully"
+            kubectl scale --replicas=0 deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
+            break
+        fi
+        sleep 60 
+
+done
+
+
